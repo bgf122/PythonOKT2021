746ff935af4d240b076f971179eaa91cde30c6f6
Bgf122 <smage90@hotmail.com>
2021-09-15 21:00:31 +0300

    added coverage

--- test_postinumerot.py
+++ test_postinumerot.py
@@ -13,16 +13,6 @@ def test_useampi_postinumero():
     assert len(porvoo) == 9
 
 
-def test_smart_post_no_fix():
-    smartpost = postinumerot.suorita('smartpost')
-    smart_post = postinumerot.suorita('smart post')
-    smartpsot = postinumerot.suorita('smartpsot')
-
-    # total 812+4+3 = 819
-    assert len(smartpost) == 812 and len(
-        smart_post) == 4 and len(smartpsot) == 3
-
-
 def test_smart_post_final():
     smartpost = postinumerot.suorita('smartpost')
 
